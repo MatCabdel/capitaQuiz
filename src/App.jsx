8dee90bff15100607b9f2b9223291b7e5690ead9
Mathieu Chauveau <chauveau.math@gmail.com>
2023-11-07 21:28:36 +0100

    Logo + debut axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Countries from "./components/Countries";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-    <Countries />
+      <Header />
+      <Countries />
     </>
   );
 }
--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 function Countries() {
  
-
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then
+  })
   return (
     <>
-    <h1>Hello World</h1>
     </>
   )
 }
