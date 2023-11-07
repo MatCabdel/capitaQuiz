eb527224b6061d89ff198ee52e198d7d6a435f91
Mathieu Chauveau <chauveau.math@gmail.com>
2023-11-07 21:58:56 +0100

    country state en cours

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,15 +1,26 @@
 import { useEffect } from "react";
 import axios from "axios";
+import { useState } from "react";
 
-function Countries() {
- 
+const Countries = () => {
+  const [data, setData] = useState();
+
   useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all").then
-  })
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => setData(response.data));
+  }, []);
+
   return (
     <>
+      <h1>COUNTRIES</h1>
+      <ul>
+        {/* {data.map((country) => (
+      <li key={country.name.commons}>{country.translate.fra.commons}</li>
+    ))} */}
+      </ul>
     </>
-  )
-}
+  );
+};
 
 export default Countries;
